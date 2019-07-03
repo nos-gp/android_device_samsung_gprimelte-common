8ac88a29e4267dda1d4b6cac225f1e5b62202681
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-07-03 10:12:52 +0700

    Update and rename lotus.mk to viper.mk

--- viper.mk
+++ viper.mk
@@ -5,8 +5,10 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
-# Inherit some common Lotus stuff
-$(call inherit-product, vendor/lotus/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
